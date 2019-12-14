08b1168551cb8a42fbb9f8c5a79cb83a927d0837
Olli Karppinen <olli.karppinen@relex.fi>
2019-12-14 15:27:01 +0200

    Updates test app

--- test/index.tsx
+++ test/index.tsx
@@ -1,17 +1,8 @@
 import Reakt from '../src/Reakt'
 const React = Reakt
 
-// const element = createElement(
-//   'div',
-//   { id: 'foo' },
-//   createElement('a', { style: 'color: blue;' }, 'foo'),
-//   createElement('b', null, 'bar')
-// )
-
-// const container = document.getElementById('root')
-// render(element, container)
 const names = ['foo', 'bar', 'baz']
-const NameList = () => (<ul>{names.map(name => (<li>{name}</li>))</ul>)
+const NameList = () => (<ul>{names.map((name: any) => (<li>{name}</li>))</ul>)
 const App = (props: any) => (
   <div>
     <h1>Hi {props.name}</h1>
